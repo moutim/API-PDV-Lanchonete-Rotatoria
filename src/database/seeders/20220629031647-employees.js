cd0b2a026fdf32484f97ae751e1f4c0b0184cf96
Vitor <moutimg@gmail.com>
2022-06-29 03:18:54 -0300

    refactor: adiciona senha criptografada nos seeders

--- src/database/seeders/20220629031647-employees.js
+++ src/database/seeders/20220629031647-employees.js
@@ -5,12 +5,12 @@ module.exports = {
     await queryInterface.bulkInsert('Employees', [
       {
         user: 'vitormoutim',
-        password: '12345',
+        password: '$2b$10$9cKqGXT07m6kcunbQVn4bOtNrynjkM8.75c6LflwDXn8FZojWAesK',
         levelId: 1
       },
       {
         user: 'hugomoutim',
-        password: '12345',
+        password: '$2b$10$xUIzvExPUX/5Cyfrgv6M0ufR5Eg99hmTfr8qxrvWL1v9tG0BuHwt6',
         levelId: 2
       }
     ], {});
