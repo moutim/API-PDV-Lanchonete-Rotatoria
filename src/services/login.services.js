af7f651431a8f3aecf355357497c91b89a73d93e
Vitor <moutimg@gmail.com>
2022-06-29 03:19:39 -0300

    feat: cria funcao na camada de services para realizar login e gerar um token de autenticacao

---
+++ src/services/login.services.js
@@ -0,0 +1,23 @@
+const { Employees } = require('../database/models');
+const bcrypt = require('../utils/bcrypt');
+const JWT = require('../utils/JWT');
+
+const login = async ({ user, password }) => {
+  const employee = await Employees.findOne({ where: { user } });
+
+  if (!employee) {
+    throw new Error(JSON.stringify({ status: 404, message: 'Usuario nao encontrado.' }));
+  }
+
+  if (!bcrypt.comparePassword(password, employee.dataValues.password)) {
+    throw new Error(JSON.stringify({ status: 404, message: 'Senha incorreta.' }));
+  }
+
+  const token = JWT.generateToken(JSON.stringify({ user }));
+
+  return token;
+};
+
+module.exports = {
+  login
+}
