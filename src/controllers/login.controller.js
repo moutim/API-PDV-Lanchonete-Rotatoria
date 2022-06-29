a982c24ef5df27d7bf8d7591b5a58129faae1470
Vitor <moutimg@gmail.com>
2022-06-29 03:19:25 -0300

    feat: cria funcao na camada de controller para realizar login e gerar um token de autenticacao

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,5 +1,9 @@
-const login = (req, res) => {
-  res.status(200).json({ message: 'TESTANDO' });
+const service = require('../services/login.services');
+
+const login = async (req, res) => {
+  const token = await service.login(req.body);
+
+  res.status(200).json({ token });
 };
 
 module.exports = {
