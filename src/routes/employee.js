689151021da0c15177733109ae2d24165e133df7
unknown <moutimg@gmail.com>
2022-07-02 19:33:13 -0300

    feat: cria rota GET e POST para '/empoloyee'

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -3,7 +3,10 @@ const express = require('express');
 const routes = express.Router();
 
 const controller = require('../controllers/employees.controller');
+const middlewares = require('../middlewares');
 
-routes.post('/', controller.createEmployee);
+routes.get('/', controller.getEmployees);
+
+routes.post('/', middlewares.validateCreateEmployee, controller.createEmployee);
 
 module.exports = routes;
