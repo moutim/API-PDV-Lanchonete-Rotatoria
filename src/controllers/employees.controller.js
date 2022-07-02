537f5551828f83081ab8cee15f1068685d0a6fe4
unknown <moutimg@gmail.com>
2022-07-02 19:37:19 -0300

    feat: cria funcao na camada de controller para buscar um funcionario pelo id

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -6,6 +6,12 @@ const getEmployees = async (req, res) => {
   res.status(200).json(employees);
 };
 
+const getEmployee = async (req, res) => {
+  const employee = await service.getEmployee(req.params.id);
+
+  res.status(200).json(employee);
+};
+
 const createEmployee = async (req, res) => {
   const employee = await service.createEmployee(req.body);
 
@@ -15,4 +21,5 @@ const createEmployee = async (req, res) => {
 module.exports = {
   createEmployee,
   getEmployees,
+  getEmployee,
 };
