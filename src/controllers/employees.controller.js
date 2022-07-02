068c49760ce6ab9fdf49685f1f40030b4f349a8a
unknown <moutimg@gmail.com>
2022-07-02 19:32:11 -0300

    feat: cria funcao para buscar todos os funcionarios cadastrados na tabela Employee

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,5 +1,11 @@
 const service = require('../services/employees.service');
 
+const getEmployees = async (req, res) => {
+  const employees = await service.getEmployees();
+
+  res.status(200).json(employees);
+};
+
 const createEmployee = async (req, res) => {
   const employee = await service.createEmployee(req.body);
 
@@ -8,4 +14,5 @@ const createEmployee = async (req, res) => {
 
 module.exports = {
   createEmployee,
+  getEmployees,
 };
