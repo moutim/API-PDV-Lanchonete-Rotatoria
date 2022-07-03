4103d9d57d8066f223b6fb9cfcea6806d08de7e1
unknown <moutimg@gmail.com>
2022-07-02 21:35:34 -0300

    feat: cria funcao na camada de controller para atualizar informacoes de um produto na tabela Products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -18,8 +18,15 @@ const createProduct = async (req, res) => {
   res.status(200).json(result);
 };
 
+const updateProduct = async (req, res) => {
+  const result = await service.updateProduct(req.params.id, req.body);
+
+  res.status(200).json(result);
+};
+
 module.exports = {
   getProducts,
   getProduct,
   createProduct,
+  updateProduct,
 };
