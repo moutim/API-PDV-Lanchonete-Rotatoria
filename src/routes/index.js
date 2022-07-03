b9984312853ed1e76089c99b9a2c688de00626ff
unknown <moutimg@gmail.com>
2022-07-02 22:06:06 -0300

    feat: adiciona validacao de token nas rotas 'employee' e 'products'

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const routes = express.Router();
 
+const middlewares = require('../middlewares');
+
 routes.use('/login', require('./login'));
 
-routes.use('/employee', require('./employee'));
+routes.use('/employee', middlewares.validateToken, require('./employee'));
 
-routes.use('/products', require('./products'));
+routes.use('/products', middlewares.validateToken, require('./products'));
 
 module.exports = routes;
