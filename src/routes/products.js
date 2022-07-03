2fc2029b234ce161751ef8fc6511c2d8135e8765
unknown <moutimg@gmail.com>
2022-07-02 21:31:15 -0300

    feat: cria rota POST '/products' para criar um novo produto na tabela Products'

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,8 +4,12 @@ const routes = express.Router();
 
 const controller = require('../controllers/products.controller');
 
+const middlewares = require('../middlewares');
+
 routes.get('/', controller.getProducts);
 
 routes.get('/:id', controller.getProduct);
+
+routes.post('/', middlewares.validateCreateProduct, controller.createProduct);
 
 module.exports = routes;
