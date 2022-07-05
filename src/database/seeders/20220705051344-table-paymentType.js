6ce88102170f45fd273637f0d2e1a775865fba52
unknown <moutimg@gmail.com>
2022-07-05 03:31:40 -0300

    fix: fix typo

--- src/database/seeders/20220705051344-table-paymentType.js
+++ src/database/seeders/20220705051344-table-paymentType.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('PaymentType', [
+    await queryInterface.bulkInsert('PaymentTypes', [
       {
         type: 'debit',
       },
@@ -19,6 +19,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('PaymentType', null, {});
+    await queryInterface.bulkDelete('PaymentTypes', null, {});
   }
 };
