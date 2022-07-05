ede94a5905e361861575a814c546550b5ad78546
unknown <moutimg@gmail.com>
2022-07-05 02:25:49 -0300

    feat: cria model para a tabela PaymentType

---
+++ src/database/models/PaymentType.js
@@ -0,0 +1,18 @@
+const paymentType = (sequelize, DataTypes) => {
+  const tablePaymentType = sequelize.define("PaymentType", 
+    {
+      type: DataTypes.STRING
+    },
+    { timestamps: false }
+  );
+
+  tablePaymentType.associate = (models) => {
+    tablePaymentType.hasOne(models.Sales, {
+      foreignKey: 'paymentId', as: 'sale'
+    });
+  };
+
+  return tablePaymentType;
+};
+
+module.exports = paymentType;
