b411c189727eea3d617807da53f40a95be4aa374
unknown <moutimg@gmail.com>
2022-07-05 03:36:19 -0300

    feat: cria funcao na camada controller para buscar venda pelo id na  tabela Sales

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -6,6 +6,13 @@ const getSales = async (req, res) => {
   res.status(200).json(sales);
 };
 
+const getSale = async (req, res) => {
+  const sale = await service.getSale(req.params.id);
+
+  res.status(200).json(sale);
+};
+
 module.exports = {
   getSales,
+  getSale,
 };
