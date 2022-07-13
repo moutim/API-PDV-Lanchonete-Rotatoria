7214d5c76371a57edcaf7abc6726bc4a6d5823d5
unknown <moutimg@gmail.com>
2022-07-13 01:24:25 -0300

    feat: instala e usa cors na aplicacao

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 require('express-async-errors');
 
+const cors = require('cors');
+
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 const routes = require('./src/routes');
 
