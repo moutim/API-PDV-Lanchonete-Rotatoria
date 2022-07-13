c8e7dc993c323c362e6ead8fba3aafa6eb64f34f
unknown <moutimg@gmail.com>
2022-07-13 19:49:25 -0300

    fix: arruma erros de bug na requisicao

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', '*');
+  app.use(cors());
+  next();
+});
 
 const routes = require('./src/routes');
 
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,12 +1,12 @@
-const errorHandler = (error, req, res, next) => {
+/* eslint-disable no-unused-vars */
+const errorHandler = (error, req, res, _next) => {
   try {
     const erro = JSON.parse(error.message);
     res.status(erro.status).json({ message: erro.message });
+    return;
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
-
-  next();
 };
 
 module.exports = errorHandler;
